2da98e48e8c0419e9631dcb671311ea3550585cf
Luxannn <146860983+Luxannn@users.noreply.github.com>
2023-10-03 22:46:35 +0500

    Add files via upload

---
+++ credit_scoring_using_logistic_regression.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pickle
+import pandas as pd
+import streamlit as st
+
+from PIL import Image
+
+def main():
+    st.title("Credit Scoring")
+
+if __name__=='__main__':
+    main()
